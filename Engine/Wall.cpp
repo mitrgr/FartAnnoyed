a4ba6634a7211875fc961c34746d7d36e2f8786c
Mikael Oskar Engelun <Mikael Oskar Engelun@DESKTOP-PT8OEJ2>
2017-07-20 11:06:05 +0200

    Check if we are inside box

--- Engine/Wall.cpp
+++ Engine/Wall.cpp
@@ -13,5 +13,22 @@ void Wall::Draw(Graphics& gfx) const
 	gfx.DrawBox(int(GetUL().x), int(GetUL().y) ,int( GetDR().x), int(GetDR().y), Col);
 }
 
+bool Wall::OutsideLeft(Rect R)
+{
+	return R.GetDL().x <= GetDL().x;
+}
+
+bool Wall::OutsideRight(Rect R)
+{
+	return R.GetDR().x >= GetDR().x;
+}
+bool Wall::OutsideTop(Rect R)
+{
+	return R.GetUR().y <= GetUR().y;
+}
+bool Wall::OutsideBottom(Rect R)
+{
+	return R.GetDR().y >= GetDR().y;
+}
 
 
--- Engine/Wall.h
+++ Engine/Wall.h
@@ -2,12 +2,23 @@
 #include "Rect.h"
 #include "Graphics.h"
 
-class Wall: public Rect
+class Wall: protected Rect
 {
 public:
 	Wall(float x, float y, const float h, const float w);
 	
 	void Draw(Graphics& gfx) const;
+
+	bool Wall::OutsideLeft(Rect R);
+	
+	bool Wall::OutsideRight(Rect R);
+	
+	bool Wall::OutsideTop(Rect R);
+	
+	bool Wall::OutsideBottom(Rect R);
+	
+
+private:
 
 
 };
